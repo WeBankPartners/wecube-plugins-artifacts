b1419a42b48cd58ace5ac1d4808005061fffc470
zhangsenfeng <729346129@qq.com>
2019-11-22 16:03:11 +0800

    :tada: #1 artifacts ui

---
+++ artifacts-ui/src/api/base.js
@@ -0,0 +1,65 @@
+import Vue from "vue";
+import axios from "axios";
+export const baseURL = window.request ? "/weartifacts/weartifacts/ui/v2" : "/weartifacts/ui/v2";
+export const req = axios.create({
+  withCredentials: true,
+  baseURL,
+  timeout: 50000
+});
+
+const throwError = res => new Error(res.message || "error");
+req.interceptors.response.use(
+  res => {
+    if (res.status === 200) {
+      if (res.data.statusCode.startsWith("ERR")) {
+        const errorMes = Array.isArray(res.data.data)
+          ? res.data.data.map(_ => _.errorMessage).join("<br/>")
+          : res.data.statusMessage;
+        Vue.prototype.$Notice.error({
+          title: "Error",
+          desc: errorMes,
+          duration: 0
+        });
+      }
+      if (!res.headers["username"]) {
+        window.location.href = "/weartifacts/logout";
+      }
+      return {
+        ...res.data,
+        user: res.headers["username"] || " - "
+      };
+    } else {
+      return {
+        data: throwError(res)
+      };
+    }
+  },
+  res => {
+    const { response } = res;
+    Vue.prototype.$Notice.error({
+      title: "error",
+      desc:
+        (response.data &&
+          "status:" +
+            response.data.status +
+            "<br/> error:" +
+            response.data.error +
+            "<br/> message:" +
+            response.data.message) ||
+        "error"
+    });
+    return new Promise((resolve, reject) => {
+      resolve({
+        data: throwError(res)
+      });
+    });
+  }
+);
+
+function setHeaders(obj) {
+  Object.keys(obj).forEach(key => {
+    req.defaults.headers.common[key] = obj[key];
+  });
+}
+
+export { setHeaders };
---
+++ artifacts-ui/src/main.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import ViewUI from "view-design";
+import "view-design/dist/styles/iview.css";
+
+Vue.config.productionTip = false
+
+Vue.use(ViewUI, {
+  transfer: true,
+  size: "default"
+});
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
