5e92a1ab390094ecda941bdb37b8c4aae263e079
Jordan Zhang <jordanzhang@webank.com>
2019-11-26 16:31:28 +0800

    #3 Disable database as no use

--- artifacts-core/src/main/java/com/webank/plugins/artifacts/Application.java
+++ artifacts-core/src/main/java/com/webank/plugins/artifacts/Application.java
@@ -1,7 +1,9 @@
 package com.webank.plugins.artifacts;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 import com.webank.plugins.artifacts.config.SpringAppConfig;
@@ -9,6 +11,7 @@ import com.webank.plugins.artifacts.config.SpringWebConfig;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 @SpringBootApplication
 @EnableSwagger2
 public class Application extends AbstractAnnotationConfigDispatcherServletInitializer {
