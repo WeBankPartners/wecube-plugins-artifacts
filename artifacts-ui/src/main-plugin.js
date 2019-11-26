b442738017bc5072ad30c57faae832a1d1ae3d67
Jordan Zhang <jordanzhang@webank.com>
2019-11-26 18:01:34 +0800

    #3 Fix the issue of UI components register

--- artifacts-ui/src/main-plugin.js
+++ artifacts-ui/src/main-plugin.js
@@ -3,13 +3,9 @@ import router from "./router-plugin";
 import ArtifactsSimpleTable from "../src/components/simple-table.vue";
 import ArtifactsAttrInput from "../src/components/attr-input.vue";
 
-Vue.component("ArtifactsSimpleTable", ArtifactsSimpleTable);
-Vue.component("ArtifactsAttrInput", ArtifactsAttrInput);
+window.component("ArtifactsSimpleTable", ArtifactsSimpleTable);
+window.component("ArtifactsAttrInput", ArtifactsAttrInput);
 
 Vue.config.productionTip = false
-
-//window.component("WeCMDBSelect", WeCMDBSelect); -->  Vue.component("WeCMDBSelect", WeCMDBSelect)
-//window.use(iview)  -->  Vue.use(iview)
-
 
 window.addRoutes && window.addRoutes(router, "plugin-name");
