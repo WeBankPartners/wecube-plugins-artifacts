80aa2db01ffaee310291ed7eeacab4f0300e2b9e
Jordan Zhang <jordanzhang@webank.com>
2019-11-28 12:18:12 +0800

    #10 Add the configuration for connecting wecube platform

--- build/start.sh
+++ build/start.sh
@@ -3,8 +3,9 @@ mkdir -p /log
 java -jar /application/wecube-plugins-artifacts.jar  \
 --server.address=0.0.0.0 \
 --server.port=8081 \
---plugins.wecmdb-server-url=$1 \
---plugins.saltstack-server-url=$2 \
---plugins.artifacts-s3-server-url=$3 \
---plugins.artifacts-s3-access-key=$4 \
---plugins.artifacts-s3-secret-key=$5 >>/log/wecube-plugins-artifacts.log 
+--plugins.wecube-platform-server-url=$1 \
+--plugins.wecmdb-server-url=$2 \
+--plugins.saltstack-server-url=$3 \
+--plugins.artifacts-s3-server-url=$4 \
+--plugins.artifacts-s3-access-key=$5 \
+--plugins.artifacts-s3-secret-key=$6 >>/log/wecube-plugins-artifacts.log 
