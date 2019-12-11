9795ff712f0b257959a99fcb8ef2b0d910f9ed9d
Jordan Zhang <jordanzhang@webank.com>
2019-12-11 18:25:58 +0800

    #27 Add token for accessing wecube platform

--- artifacts-core/src/main/java/com/webank/plugins/artifacts/interceptor/RestTemplateInterceptor.java
+++ artifacts-core/src/main/java/com/webank/plugins/artifacts/interceptor/RestTemplateInterceptor.java
@@ -2,6 +2,7 @@ package com.webank.plugins.artifacts.interceptor;
 
 import java.io.IOException;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpRequest;
 import org.springframework.http.client.ClientHttpRequestExecution;
@@ -9,14 +10,20 @@ import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.stereotype.Component;
 
+import com.webank.plugins.artifacts.commons.ApplicationProperties;
+
 @Component
 public class RestTemplateInterceptor implements ClientHttpRequestInterceptor {
+    @Autowired
+    private ApplicationProperties applicationProperties;
+
     @Override
     public ClientHttpResponse intercept(HttpRequest request, byte[] body, ClientHttpRequestExecution execution)
             throws IOException {
         HttpHeaders headers = request.getHeaders();
         String username = UsernameStorage.getIntance().get();
         headers.add("username", username);
+        headers.add("Authorization", applicationProperties.getWecubePlatformToken());
         return execution.execute(request, body);
     }
 }
