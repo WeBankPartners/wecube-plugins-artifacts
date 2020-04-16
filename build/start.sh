812ad2294aae32d00c40cdcc5685cc5d213f2cac
Jordan Zhang <jordanzhang@webank.com>
2020-04-16 17:40:07 +0800

    #86 Adjust register and start script

--- build/start.sh
+++ build/start.sh
@@ -3,9 +3,7 @@ mkdir -p /log
 java -jar /application/wecube-plugins-artifacts.jar  \
 --server.address=0.0.0.0 \
 --server.port=8081 \
---plugins.wecube-platform-server-url=$1 \
---plugins.wecmdb-server-url=$2 \
---plugins.saltstack-server-url=$3 \
---plugins.artifacts-s3-server-url=$4 \
---plugins.artifacts-s3-access-key=$5 \
---plugins.artifacts-s3-secret-key=$6 >>/log/wecube-plugins-artifacts.log 
+--plugins.wecube-gateway-server-url=$1 \
+--plugins.artifacts-s3-server-url=$2 \
+--plugins.artifacts-s3-access-key=$3 \
+--plugins.artifacts-s3-secret-key=$4 >>/log/wecube-plugins-artifacts.log 
