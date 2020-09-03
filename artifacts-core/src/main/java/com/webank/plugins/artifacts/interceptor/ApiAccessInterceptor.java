923d39edcd3fdf77c8c781dceb47c138db0cda63
gavin2lee <gavin2lee@163.com>
2020-09-03 12:19:26 +0800

    #164 add token interceptor

--- artifacts-core/src/main/java/com/webank/plugins/artifacts/interceptor/ApiAccessInterceptor.java
+++ artifacts-core/src/main/java/com/webank/plugins/artifacts/interceptor/ApiAccessInterceptor.java
@@ -1,13 +1,15 @@
 package com.webank.plugins.artifacts.interceptor;
 
+import java.security.Principal;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import com.webank.plugins.artifacts.constant.ArtifactsConstants;
-import com.webank.plugins.artifacts.utils.JwtUtils;
 
 @Component
 public class ApiAccessInterceptor implements HandlerInterceptor {
@@ -15,13 +17,15 @@ public class ApiAccessInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
         String authorization = request.getHeader(ArtifactsConstants.KEY_OF_TOKEN);
-        request.setAttribute(ArtifactsConstants.UPLOAD_NAME, JwtUtils.getAuthUserName(authorization));
-        if (authorization != null) {
+        
+        Principal userPrincipal = request.getUserPrincipal();
+        if (userPrincipal != null && (userPrincipal instanceof Authentication)) {
+            
             response.setHeader(ArtifactsConstants.KEY_OF_TOKEN, authorization);
             AuthorizationStorage.getIntance().set(authorization);
-        } else {
-            response.setHeader(ArtifactsConstants.KEY_OF_TOKEN, null);
-            AuthorizationStorage.getIntance().set(ArtifactsConstants.KEY_OF_TOKEN);
+            
+            Authentication auth = (Authentication)userPrincipal;
+            request.setAttribute(ArtifactsConstants.UPLOAD_NAME, auth.getName());
         }
         return true;
     }
