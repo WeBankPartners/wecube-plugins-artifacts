9d84498df4485403dc0f5e9c004748f7a5e9152a
royl88 <roywu@webank.com>
2021-09-08 11:42:18 +0800

    #286 env for cleanup

--- artifacts-corepy/artifacts_corepy/common/wecube.py
+++ artifacts-corepy/artifacts_corepy/common/wecube.py
@@ -6,9 +6,10 @@ artifacts_corepy.common.wecube
 本模块提供项目WeCube Client（Proxy）
 
 """
+import base64
 import logging
+import random
 
-from artifacts_corepy.common import exceptions
 from talos.core import config
 from talos.core.i18n import _
 from artifacts_corepy.common import utils
@@ -17,38 +18,45 @@ LOG = logging.getLogger(__name__)
 CONF = config.CONF
 
 
-class WeCubeClient(object):
+def encrypt(message, rsa_key):
+    import M2Crypto.RSA
+    template = '''-----BEGIN PRIVATE KEY-----
+%s
+-----END PRIVATE KEY-----'''
+    key_pem = template % rsa_key
+    privat_key = M2Crypto.RSA.load_key_string(key_pem.encode())
+    ciphertext = privat_key.private_encrypt(message.encode(), M2Crypto.RSA.pkcs1_padding)
+    encrypted_message = base64.b64encode(ciphertext).decode()
+    return encrypted_message
+
+
+class WeCubeClient(utils.ClientMixin):
     """WeCube Client"""
     def __init__(self, server, token):
         self.server = server.rstrip('/')
         self.token = token
 
-    def build_headers(self):
-        return {'Authorization': 'Bearer ' + self.token}
-
-    def check_response(self, resp_json):
-        if resp_json['status'] != 'OK':
-            # 当创建/更新条目错误，且仅有一个错误时，返回内部错误信息
-            if isinstance(resp_json.get('data', None), list) and len(resp_json['data']) == 1:
-                if 'message' in resp_json['data'][0]:
-                    raise exceptions.PluginError(message=resp_json['data'][0]['message'])
-            raise exceptions.PluginError(message=resp_json['message'])
-
-    def get(self, url, param=None):
-        LOG.info('GET %s', url)
-        LOG.debug('Request: query - %s, data - None', str(param))
-        resp_json = utils.RestfulJson.get(url, headers=self.build_headers(), params=param)
-        LOG.debug('Response: %s', str(resp_json))
-        self.check_response(resp_json)
-        return resp_json
-
-    def post(self, url, data, param=None):
-        LOG.info('POST %s', url)
-        LOG.debug('Request: query - %s, data - %s', str(param), str(data))
-        resp_json = utils.RestfulJson.post(url, headers=self.build_headers(), params=param, json=data)
-        LOG.debug('Response: %s', str(resp_json))
-        self.check_response(resp_json)
-        return resp_json
+    def login_subsystem(self, set_self=True):
+        '''client = WeCubeClient('http://ip:port', None)
+           token = client.login_subsystem()
+           # use your access token
+        '''
+        sequence = 'abcdefghijklmnopqrstuvwxyz1234567890'
+        nonce = ''.join(random.choices(sequence, k=4))
+        url = self.server + '/auth/v1/api/login'
+        password = encrypt('%s:%s' % (CONF.wecube.sub_system_code, nonce), CONF.wecube.sub_system_key)
+        data = {
+            "password": password,
+            "username": CONF.wecube.sub_system_code,
+            "nonce": nonce,
+            "clientType": "SUB_SYSTEM"
+        }
+        resp_json = self.post(url, data)
+        for token in resp_json['data']:
+            if token['tokenType'] == 'accessToken':
+                if set_self:
+                    self.token = token['token']
+                return token['token']
 
     def update(self, url_path, data):
         url = self.server + url_path
@@ -56,4 +64,4 @@ class WeCubeClient(object):
 
     def retrieve(self, url_path):
         url = self.server + url_path
-        return self.post(url, {})
+        return self.get(url)
--- artifacts-corepy/artifacts_corepy/server/wsgi_server.py
+++ artifacts-corepy/artifacts_corepy/server/wsgi_server.py
@@ -9,65 +9,15 @@ artifacts_corepy.server.wsgi_server
 
 from __future__ import absolute_import
 
-import base64
 import json
 import os
 import os.path
 
-from artifacts_corepy.common import utils as plugin_utils
 from artifacts_corepy.middlewares import auth
 from artifacts_corepy.middlewares import permission
-from Crypto import Random
-from Crypto.Cipher import PKCS1_v1_5 as Cipher_pkcs1_v1_5
-from Crypto.PublicKey import RSA
-from talos.core import config, utils
+from talos.core import utils
 from talos.server import base
-
-# @config.intercept('db_password', 'other_password')
-# def get_password(value, origin_value):
-#     """value为上一个拦截器处理后的值（若此函数为第一个拦截器，等价于origin_value）
-#        origin_value为原始配置文件的值
-#        没有拦截的变量talos将自动使用原始值，因此定义一个拦截器是很关键的
-#        函数处理后要求必须返回一个值
-#     """
-#     # 演示使用不安全的base64，请使用你认为安全的算法进行处理
-#     return base64.b64decode(origin_value)
-
-RSA_KEY_PATH = '/certs/rsa_key'
-
-
-def decrypt_rsa(secret_key, encrypt_text):
-    rsakey = RSA.importKey(secret_key)
-    cipher = Cipher_pkcs1_v1_5.new(rsakey)
-    random_generator = Random.new().read
-    text = cipher.decrypt(plugin_utils.b64decode_key(encrypt_text), random_generator)
-    return text.decode('utf-8')
-
-
-@config.intercept('upload_enabled', 'upload_nexus_enabled', 'ci_typeid_system_design', 'ci_typeid_unit_design',
-                  'ci_typeid_diff_config', 'ci_typeid_deploy_package', 'encrypt_variable_prefix',
-                  'file_variable_prefix', 'default_special_replace', 'artifact_field', 's3_access_key', 's3_secret_key',
-                  'nexus_server', 'nexus_repository', 'nexus_username', 'nexus_password', 'local_nexus_server',
-                  'local_nexus_repository', 'local_nexus_username', 'local_nexus_password', 'gateway_url',
-                  'diff_conf_extension', 'variable_expression', 'jwt_signing_key', 'use_remote_nexus_only',
-                  'nexus_sort_as_string', 'local_nexus_connector_port', 'nexus_connector_port', 'platform_timezone',
-                  'system_design_view')
-def get_env_value(value, origin_value):
-    prefix = 'ENV@'
-    encrypt_prefix = 'RSA@'
-    if value.startswith(prefix):
-        env_name = value[len(prefix):]
-        new_value = os.getenv(env_name, default='')
-        if new_value.startswith(encrypt_prefix):
-            certs_path = RSA_KEY_PATH
-            if os.path.exists(certs_path) and os.path.isfile(certs_path):
-                with open(certs_path) as f:
-                    new_value = decrypt_rsa(f.read(), new_value[len(encrypt_prefix):])
-            else:
-                raise ValueError('keys with "RSA@", but rsa_key file not exists')
-        return new_value
-    return value
-    # raise ValueError('config intercepter of get_env_value support "ENV@" only')
+from artifacts_corepy.server import base as artifacts_base
 
 
 def error_serializer(req, resp, exception):
@@ -89,3 +39,4 @@ application = base.initialize_server('artifacts_corepy',
                                                              '/etc/artifacts_corepy/artifacts_corepy.conf.d'),
                                      middlewares=[auth.JWTAuth(), permission.Permission()])
 application.set_error_serializer(error_serializer)
+application.req_options.auto_parse_qs_csv = True
