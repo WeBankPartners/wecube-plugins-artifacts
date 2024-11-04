39169f52f5db4954c6ae6dbbcf6154e60031fc4b
gaohongsong <gaohongsong@qq.com>
2024-11-04 16:55:21 +0800

    feat: add artifacts.diff-conf-templates and auth app

---
+++ artifacts-corepy/artifacts_corepy/apps/auth/route.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+
+from artifacts_corepy.apps.auth import controller
+
+
+def add_routes(api):
+    # 不需要登陆
+    api.add_route('/artifacts/v1/login', controller.Token())
+    api.add_route('/artifacts/v1/refresh-token', controller.TokenRefresh())
+    # 登陆但不需要权限校验
+    api.add_route('/artifacts/v1/user-menus', controller.UserMenus())
+    api.add_route('/artifacts/v1/user-password', controller.UserPassword())
+    # 登陆且经过权限校验
+    api.add_route('/artifacts/v1/users', controller.User())
+    api.add_route('/artifacts/v1/users/{rid}', controller.UserItem())
+    api.add_route('/artifacts/v1/users/{rid}/reset-password', controller.UserItemResetPassword())
+    api.add_route('/artifacts/v1/users/{rid}/menus', controller.UserItemMenu())
+    api.add_route('/artifacts/v1/users/{rid}/roles', controller.UserItemRole())
+    api.add_route('/artifacts/v1/roles', controller.Role())
+    api.add_route('/artifacts/v1/roles/{rid}', controller.RoleItem())
+    api.add_route('/artifacts/v1/roles/{rid}/menus', controller.RoleItemMenu())
+    api.add_route('/artifacts/v1/roles/{rid}/users', controller.RoleItemUser())
+    api.add_route('/artifacts/v1/menus', controller.Menu())
+    # api.add_route('/artifacts/v1/menus/{rid}', controller.MenuItem())
--- artifacts-corepy/artifacts_corepy/db/models.py
+++ artifacts-corepy/artifacts_corepy/db/models.py
@@ -2,8 +2,10 @@
 from __future__ import absolute_import
 
 from talos.db.dictbase import DictBase
-from sqlalchemy import Column, DateTime, ForeignKey, String, text, Text, func, create_engine, UniqueConstraint
-from sqlalchemy.dialects.mysql import BIGINT, TINYINT
+from sqlalchemy import (
+    Column, DateTime, ForeignKey, String, text, Text, func, create_engine, UniqueConstraint,
+)
+from sqlalchemy.dialects.mysql import BIGINT, TINYINT, INTEGER
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -60,6 +62,109 @@ class DiffConfTemplateRole(Base, DictBase):
 
     def __repr__(self):
         return "{0}:{1}".format(self.role, self.diff_conf_template_id)
+
+
+class SysMenu(Base, DictBase):
+    __tablename__ = 'sys_menu'
+    attributes = [
+        'id', 'display_name', 'url', 'seq_no', 'parent', 'is_active', 'created_by', 'created_time', 'updated_by',
+        'updated_time'
+    ]
+    summary_attributes = ['id', 'display_name', 'url', 'seq_no', 'parent', 'is_active']
+
+    id = Column(String(36), primary_key=True, comment='主键')
+    display_name = Column(String(64), comment='显示名')
+    url = Column(String(255), comment='访问路径')
+    seq_no = Column(INTEGER(11), server_default=text("'0'"), comment='排序号')
+    parent = Column(String(36), comment='父菜单')
+    is_active = Column(String(8), server_default=text("'yes'"), comment='状态')
+    created_by = Column(String(36))
+    created_time = Column(DateTime)
+    updated_by = Column(String(36))
+    updated_time = Column(DateTime)
+
+    roles = relationship("SysRole", secondary="sys_role_menu", back_populates="menus", uselist=True, viewonly=True)
+
+
+class SysRole(Base, DictBase):
+    __tablename__ = 'sys_role'
+    attributes = [
+        'id', 'description', 'role_type', 'is_system', 'created_by', 'created_time', 'updated_by', 'updated_time',
+        'menus'
+    ]
+    summary_attributes = ['id', 'description', 'role_type', 'is_system']
+    detail_attributes = [
+        'id', 'description', 'role_type', 'is_system', 'created_by', 'created_time', 'updated_by', 'updated_time',
+        'users', 'menus'
+    ]
+
+    id = Column(String(36), primary_key=True, comment='主键')
+    description = Column(String(255), comment='描述')
+    role_type = Column(String(32), comment='角色类型')
+    is_system = Column(String(8), server_default=text("'no'"), comment='是否系统角色')
+    created_by = Column(String(36))
+    created_time = Column(DateTime)
+    updated_by = Column(String(36))
+    updated_time = Column(DateTime)
+
+    users = relationship("SysUser", secondary="sys_role_user", back_populates="roles", uselist=True, viewonly=True)
+    menus = relationship("SysMenu", secondary="sys_role_menu", back_populates="roles", uselist=True, viewonly=True)
+
+
+class SysUser(Base, DictBase):
+    __tablename__ = 'sys_user'
+    attributes = [
+        'id', 'display_name', 'password', 'salt', 'description', 'is_system', 'created_by', 'created_time',
+        'updated_by', 'updated_time'
+    ]
+    summary_attributes = ['id', 'display_name', 'description', 'is_system']
+    detail_attributes = [
+        'id', 'display_name', 'password', 'salt', 'description', 'is_system', 'created_by', 'created_time',
+        'updated_by', 'updated_time', 'roles'
+    ]
+
+    id = Column(String(36), primary_key=True, comment='主键')
+    display_name = Column(String(64), comment='显示名')
+    password = Column(String(128), comment='加密密钥')
+    salt = Column(String(36), comment='加密盐')
+    description = Column(String(255), comment='描述')
+    is_system = Column(String(8), server_default=text("'no'"), comment='是否系统用户')
+    created_by = Column(String(36))
+    created_time = Column(DateTime)
+    updated_by = Column(String(36))
+    updated_time = Column(DateTime)
+
+    roles = relationship("SysRole", secondary="sys_role_user", back_populates="users", uselist=True, viewonly=True)
+
+
+class SysRoleMenu(Base, DictBase):
+    __tablename__ = 'sys_role_menu'
+
+    id = Column(BIGINT(20), primary_key=True)
+    role_id = Column(ForeignKey('sys_role.id'), index=True, comment='角色id')
+    menu_id = Column(ForeignKey('sys_menu.id'), index=True, comment='菜单id')
+    created_by = Column(String(36))
+    created_time = Column(DateTime)
+    updated_by = Column(String(36))
+    updated_time = Column(DateTime)
+
+    menu = relationship('SysMenu')
+    role = relationship('SysRole')
+
+
+class SysRoleUser(Base, DictBase):
+    __tablename__ = 'sys_role_user'
+
+    id = Column(BIGINT(20), primary_key=True)
+    role_id = Column(ForeignKey('sys_role.id'), index=True, comment='角色id')
+    user_id = Column(ForeignKey('sys_user.id'), index=True, comment='用户id')
+    created_by = Column(String(36))
+    created_time = Column(DateTime)
+    updated_by = Column(String(36))
+    updated_time = Column(DateTime)
+
+    role = relationship('SysRole')
+    user = relationship('SysUser')
 
 
 if __name__ == '__main__':
