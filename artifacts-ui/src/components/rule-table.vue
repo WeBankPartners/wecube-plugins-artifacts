84b10c851a79b71044262c157db87ce6793558d7
pobu168 <244202969@qq.com>
2024-11-14 14:56:00 +0800

    update: diff variable template select

--- artifacts-ui/src/components/rule-table.vue
+++ artifacts-ui/src/components/rule-table.vue
@@ -1,29 +1,33 @@
 <template>
   <div>
-    <Table :data="data" class="artifact-simple-table" :columns="columns" :max-height="maxHeight" size="small"></Table>
-    <Page :current="page.currentPage" :page-size="page.pageSize" :page-size-opts="pageSizeOptions" :total="page.total" @on-change="onChange" @on-page-size-change="onPageSizeChange" show-elevator show-sizer show-total style="position: absolute;right: 36px;bottom: 36px;z-index: 11" />
+    <Table :data="data" class="artifact-simple-table" ref="table" :columns="columns" :height="maxHeight" :max-height="maxHeight" size="small"> </Table>
+    <Page :current="page.currentPage" :page-size="page.pageSize" :page-size-opts="pageSizeOptions" :total="page.total" @on-change="onChange" @on-page-size-change="onPageSizeChange" show-sizer show-total style="position: absolute;right: 36px;bottom: 60px;z-index: 11" />
   </div>
 </template>
 
 <script>
 export default {
   props: {
+    maxHeight: {
+      type: Number,
+      default: 500
+    },
     data: Array,
     columns: Array,
     pagable: {
       default: true,
       required: false
     },
-    page: {
-      type: Object,
-      default: () => {
-        return {
-          currentPage: 1,
-          pageSize: 10,
-          total: 0
-        }
-      }
-    },
+    // page: {
+    //   type: Object,
+    //   default: () => {
+    //     return {
+    //       currentPage: 1,
+    //       pageSize: 10,
+    //       total: 0
+    //     }
+    //   }
+    // },
     pageSizeOptions: {
       type: Array,
       default: () => {
@@ -37,30 +41,38 @@ export default {
   },
   data () {
     return {
-      maxHeight: 500
+      page: {
+        currentPage: 1,
+        pageSize: 10,
+        total: 0
+      }
     }
   },
   mounted () {
-    this.maxHeight = window.innerHeight - 256
+    // this.maxHeight = window.innerHeight - 400
     window.addEventListener('resize', this.handleResize)
   },
   beforeDestroy () {
     window.removeEventListener('resize', this.handleResize)
   },
   methods: {
+    setPage (page) {
+      this.page = page
+    },
     handleResize () {
-      this.maxHeight = window.innerHeight - 256
+      // this.maxHeight = window.innerHeight - 256
     },
     onChange (currentPage) {
       this.$emit('reloadTableData', {
         pageSize: this.page.pageSize,
-        startIndex: currentPage - 1
+        currentPage
       })
     },
     onPageSizeChange (pageSize) {
+      this.page.pageSize = pageSize
       this.$emit('reloadTableData', {
         pageSize: pageSize,
-        startIndex: 0
+        currentPage: 1
       })
     }
   }
@@ -68,7 +80,13 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.artifact-simple-table {
+  margin-bottom: 36px;
+}
 .artifact-simple-table ::v-deep .ivu-table-hidden .ivu-tooltip {
   width: initial !important;
+}
+.artifact-simple-table ::v-deep thead .ivu-table-cell .ivu-checkbox {
+  display: none;
 }
 </style>
