1fd4b4cdf06b6834ac73e509f60c5f60af150bc0
gaohongsong <gaohongsong@qq.com>
2024-12-03 11:20:21 +0800

    feat(artifacts-corepy): add render-variable-values

--- artifacts-corepy/artifacts_corepy/apps/variable/route.py
+++ artifacts-corepy/artifacts_corepy/apps/variable/route.py
@@ -2,9 +2,35 @@
 
 from __future__ import absolute_import
 
+from talos.core import config
+from talos.core.i18n import _
 from artifacts_corepy.apps.variable import controller
+from artifacts_corepy.common import wecube
+from artifacts_corepy.common import exceptions
+
+CONF = config.CONF
+
+
+class VariableAdapter(object):
+    def __call__(self, req, resp, action_name):
+        server = CONF.wecube.server
+        token = req.auth_token
+        client = wecube.WeCubeClient(server, token)
+        if action_name == 'render-variable-values':
+            url = '/wecmdb/api/v1/ci-data/do/Change/app_instance'
+            data = client.retrieve(url, req.json)
+            resp.json = {'code': 200, 'status': 'OK', 'data': data['data'], 'message': 'success'}
+        else:
+            raise exceptions.NotFoundError(
+                _('%(action_name)s for variable not supported') % {
+                    'action_name': action_name,
+                })
 
 
 def add_routes(api):
     api.add_route('/artifacts/api/v1/diff-conf-templates', controller.CollectionDiffConfTemplates())
     api.add_route('/artifacts/api/v1/diff-conf-templates/{rid}', controller.ItemDiffConfTemplate())
+    api.add_sink(
+        VariableAdapter(),
+        r'/artifacts/api/v1/wecmdb/(?P<action_name>[-_A-Za-z0-9]+)'
+    )
