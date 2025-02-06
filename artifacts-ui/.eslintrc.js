67b34e4e6c37c976b0aef22bf7354870d6ea77b4
pobu168 <244202969@qq.com>
2025-02-06 18:53:38 +0800

    update: .eslintrc.js

--- artifacts-ui/.eslintrc.js
+++ artifacts-ui/.eslintrc.js
@@ -7,7 +7,9 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'vue/no-parsing-error': [2, { 'x-invalid-end-tag': false }]
+    'vue/no-parsing-error': [2, { 'x-invalid-end-tag': false }],
+    'no-multi-spaces': 'off', // 禁用 no-multi-spaces 规则
+    indent: 'off'
   },
   parserOptions: {
     parser: 'babel-eslint'
